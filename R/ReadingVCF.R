0962b0b85209fac92ab3d339869ea8af8468238e
Findlay Bewicke-Copley <findlaycopley@hotmail.co.uk>
2024-09-02 12:55:37 +0100

    Added progressbar

---
+++ R/ReadingVCF.R
@@ -0,0 +1,63 @@
+#' A function read a VCF file produced by Mutect2 and Annotated by VEP
+#'
+#' @title ReadMutect2VCF
+#' @param file The path to the file to be read
+#' @param sample The sample ID (default NULL, will be computed from filename)
+#' @return A dataframe of the VCF records with the tumour calls and annotations separated
+#' @keywords VCF mutect2 VEP
+#' @export
+#' @examples
+#'
+#' ReadMutect2VCF(filepath)
+
+ReadMutect2VCF <- function(file, sample = NULL) {
+
+}
+
+#' A function read a VCF file produced by VarDict and Annotated by VEP
+#'
+#' @title ReadVarDictVCF
+#' @param file The path to the file to be read
+#' @param sample The sample ID (default NULL, will be computed from filename)
+#' @return A dataframe of the VCF records with the tumour calls and annotations separated
+#' @keywords VCF VarDict VEP
+#' @export
+#' @examples
+#'
+#' ReadVarDictVCF(filepath)
+
+ReadVarDictVCF <- function(file, sample = NULL) {
+
+}
+
+#' A function read a VCF file produced by Varscan2 and Annotated by VEP
+#'
+#' @title ReadVarscan2VCF
+#' @param file The path to the file to be read
+#' @param sample The sample ID (default NULL, will be computed from filename)
+#' @return A dataframe of the VCF records with the tumour calls and annotations separated
+#' @keywords VCF Varscan2 VEP
+#' @export
+#' @examples
+#'
+#' ReadVarscan2VCF(filepath)
+
+ReadVarscan2VCF <- function(file, sample = NULL) {
+
+}
+
+#' A function read a VCF file produced by VarDict and Annotated by VEP
+#'
+#' @title ReadVarDictVCF
+#' @param file The path to the file to be read
+#' @param sample The sample ID (default NULL, will be computed from filename)
+#' @return A dataframe of the VCF records with the tumour calls and annotations separated
+#' @keywords VCF VarDict VEP
+#' @export
+#' @examples
+#'
+#' ReadVarDictVCF(filepath)
+
+ReadVarDictVCF <- function(file, sample = NULL) {
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Variant Helpers
+
+An r package containing some functions to assist in the analysis of variant call data.
+
+## Installation
+
+```r
+remotes::install.github("OkosunLab/VatiantHelpers")
+```
