f46fc3dba6c49f5b0d4471c02ed152e5305e00c3
Findlay Bewicke-Copley <findlaycopley@hotmail.co.uk>
2024-10-04 15:14:55 +0100

    Added a class and functions to work on the class
    I have added a class to hold the data and functions to run the analysis utilizing  this class object. I have also added in functions to determine the consensus variants from multiple variant callers.

--- R/ConsensusCalling.R
+++ R/ConsensusCalling.R
@@ -1,2 +1,182 @@
+#' A function to generate an upset plot of the consensus variants.
+#'
+#' @title upset_by_caller
+#' @return upset plot of the variants called by the callers.
+#' @param object object of class VariantHelper
+#' @param category name of a column in the metadata (default: Sample)
+#' @param colours a vector of colours to label the plot by with
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' upset_by_caller(obj)
+
+upset_by_caller <- function(object, category = Sample, colours = NULL, ...) {
+    if (is.null(colours)) {
+        s.colour <- scale_fill_discrete(guide = "none")
+    } else {
+        s.colour <- scale_fill_manual(values = colours, guide = "none")
+    }
+    combine_calls(object, ...) %>%
+        dplyr::select(varID, Caller, {{category}}, Consequence) %>%
+        mutate(values = as.integer(1)) %>%
+        pivot_wider(
+            names_from = Caller,
+            values_from = values,
+            values_fill = as.integer(0)
+        ) %>%
+        as.data.frame() %>%
+        ComplexUpset::upset(
+            names(object@Callers),
+            name = "caller",
+            width_ratio = 0.2,
+            base_annotations = list(
+                'Intersection size' = intersection_size(
+                    mapping = aes(fill = {{category}})
+                ) +
+                    s.colour
+            ),
+            set_sizes = (
+                upset_set_size(geom = geom_bar(aes(
+                    fill = {{category}}, x = group
+                ),
+                width = 0.8),) +
+                    s.colour
+            ))
+}
+
+#' A function to count the number of callers that called each variant.
+#'
+#' @title count_vars_by_caller_sample
+#' @return dataframe of the number of times a variant was called
+#' @param object object of class VariantHelper
+#' @param category name of a column in the metadata (default: Sample)
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' count_vars_by_caller_sample(obj)
+
+count_vars_by_caller_sample <- function(object, category = Sample, ...) {
+    combine_calls(object, ...) %>%
+        group_by(varID, {{category}}) %>%
+        summarise(n = n(), .groups = "keep") %>%
+        mutate(matchID = paste(varID, {{category}}))
+}
 
 
+#' A function to add variants called by n callers or more to the consensus slot of the object
+#'
+#' @title add_consensus
+#' @return object of class VariantHelper with the consensus calls in the consensus slot.
+#' @param object object of class VariantHelper
+#' @param min_caller minimum number of callers needed to retain variant (default: 3)
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' add_consensus(obj)
+
+add_consensus <- function(object, min_caller = 3, ...) {
+    consensusCalls <- count_vars_by_caller_sample(object, ...) %>%
+        filter(n >= min_caller)
+    object@Consensus <-
+        filter_by_consensus(object, consensus = consensusCalls) %>%
+        dplyr::select(-any_of(
+            c(
+                .$FORMAT %>% unique() %>% lapply(str_split_1, ":") %>% unlist() %>% unique(),
+                "INFO", "INFO.Trim", "FORMAT", "NORMAL", "AF", "RDP", "ADP"
+            )
+        )) %>%
+        mutate(value = 1) %>%
+        pivot_wider(
+            names_from = Caller,
+            values_from = value,
+            values_fill = 0
+        )
+    object@CallIDs <-
+        paste(object@Consensus$varID, object@Consensus$Sample, sep = "_")
+    object@Callers <- lapply(object@Callers, function(caller, category = Sample) {
+        caller %>%
+            unite("varID", c(CHROM, POS, REF, ALT), remove = FALSE) %>%
+            filter(paste(varID, {{category}}, sep = "_") %in% object@CallIDs)
+    })
+    object
+}
+
+#' A function to retain only the varians in consensus
+#'
+#' @title filter_by_consensus
+#' @return dataframe of the variants that exist in consensus
+#' @param object object of class VariantHelper
+#' @param consensus vector of Variant IDs for consensus variants
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' filter_by_consensus(obj)
+
+filter_by_consensus <- function(object, consensus, category = Sample, ...) {
+    combine_calls(object, ...) %>%
+        filter(paste(varID, {{category}}) %in% consensus$matchID)
+}
+
+#' A function return the consensus calls from the object with the meta.data added.
+#'
+#' @title return_consensus
+#' @return dataframe of consensus calls with the data in meta.data attached.
+#' @param object object of class VariantHelper
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' return_consensus(obj)
+
+return_consensus <- function(object) {
+    left_join(object@meta.data, object@Consensus)
+}
+
+#' A function to add a summary of a statistic from the individual callers to the consensus dataframe
+#'
+#' @title add_summary
+#' @return object of class VariantHelper
+#' @param object object of class VariantHelper
+#' @param category name of a column in the metadata (default: Sample)
+#' @param stat the statistic to be used. Must exist in the dataframes for each caller (default: AF)
+#' @param FUN function to use for the summary (default: mean)
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' add_summary(obj)
+
+
+add_summary <- function(object, ...) {
+    object@Consensus <- left_join(object@Consensus,
+                                  stat_summary(object, ...)
+    )
+    object
+}
+
+#' A function to add variants called by n callers or more to the consensus slot of the object
+#'
+#' @title stat_summary
+#' @return A dataframe of the consensus variant calls with the stat summary attached.
+#' @param object object of class VariantHelper
+#' @param category name of a column in the metadata (default: Sample)
+#' @param stat the statistic to be used. Must exist in the dataframes for each caller (default: AF)
+#' @param FUN function to use for the summary (default: mean)
+#' @param min_caller minimum number of callers needed to retain variant (default: 3)
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' stat_summary(obj)
+
+stat_summary <- function(object, category = Sample, stat = AF, FUN = mean, ...){
+    name = paste(deparse(substitute(FUN)), deparse(substitute(stat)), sep = "_")
+    combine_calls(object) %>%
+        group_by(varID, {{category}}) %>%
+        summarise(stat = FUN({{stat}}),.groups = "keep") %>%
+        rename("stat" = name)
+}
---
+++ R/VariantHelperClass.R
@@ -0,0 +1,216 @@
+#' Hold variant call data
+#'
+#' This class will hold your vcf data
+#' @title VariantHelper Class
+#' @keywords VCF variants
+#' @export
+#' @examples
+#' Community()
+
+VariantHelper <- setClass(
+    "VariantHelper",
+    slots = c(
+        Callers = "list",
+        Samples = "vector",
+        Consensus = "data.frame",
+        CallIDs = "vector",
+        meta.data = "data.frame",
+        misc = "list",
+        info = "list")
+)
+
+setMethod("show", "VariantHelper",
+          function(object) {
+              cat("Object of class VariantHelper\n",
+                  "Contains calls from the following variant callers:\n",
+                  names(object@Callers),
+                  "\n",
+                  "Samples: ", length(object@Samples))
+          })
+
+
+#' A function to generate an object of class VariantHelper for a vector of paths to folders
+#'
+#' @title variant_helper_from_folder_paths
+#' @param paths a vector of paths to folders containing VCF files
+#' @return object of class VariantHelper with the VCF files loaded
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' variant_helper_from_folder_paths(Paths)
+
+variant_helper_from_folder_paths <- function(paths) {
+    Calls <- lapply(paths, function(path) {
+        process_folder(path)
+    }) %>%
+        setNames(names(paths))
+    rv <- variant_helper_from_list_of_calls(Calls, ...)
+    return(rv)
+}
+
+#' A function to generate an object of class VariantHelper for a vector of paths to folders
+#'
+#' @title variant_helper_from_list_of_calls
+#' @param Calls a list of processed VCF files
+#' @param meta.data dataframe of metadata for samples (default: NULL)
+#' @return object of class VariantHelper with the VCF info from Calls
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' variant_helper_from_list_of_calls(Calls)
+
+variant_helper_from_list_of_calls <- function(Calls, meta.data = NULL, ...) {
+    rv <- VariantHelper()
+    rv@Callers <- Calls
+    rv <- update_stats(rv)
+    if (! is.null(meta.data)) {
+        rv <- add_metadata(rv, metadata = meta.data)
+    }
+    return(rv)
+}
+
+#' A function to update the stats held by the object
+#'
+#' @title update_stats
+#' @return object of class VariantHelper with the stats updated
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' update_stats(obj)
+
+update_stats <- function(object, ...) {
+    object@Samples <- lapply(object@Callers, function(caller){
+        unique(caller$Sample)
+    }) %>% unlist() %>% unique()
+    return(object)
+}
+
+#' A wrapper to filter all VCFs loaded in obj@Callers
+#'
+#' @title update_stats
+#' @param biotype A vector of biotypes to select (default: c("protein_coding"))
+#' @param impact A vector of biotypes to select (default: c("HIGH", "MODERATE"))
+#' @param existing bool of whether to filter by dbSNP/COSMIC (default: TRUE)
+#' @param population numeric value to filter 1000g/gnomAD (default: 0.01)
+#' @param vaf numeric value to filter vaf (default: 0)
+#' @param depth numeric value to filter depth (default: 1)
+#' @param alt.depth numeric value to filter alt allele depth (default: 1)
+#' @return object of class VariantHelper with filters applied
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' filter_variants(obj, vaf = 0.5)
+
+filter_variants <- function(object, ...) {
+    object@Callers <- lapply(object@Callers, function(obj) {
+        filter_vars(obj, ...)
+    })
+    object <- update_stats(object, ...)
+    return(object)
+}
+
+#' A function to update the stats held by the object
+#'
+#' @title add_metadata
+#' @return object of class VariantHelper with metadata added
+#' @param metadata dataframe of sample metadata
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' add_metadata(obj, metadata)
+
+add_metadata <- function(object, metadata, ...) {
+    if (sum(object@Samples %in% metadata$Sample) == length(object@Samples)) {
+        object@meta.data <- metadata
+    } else {
+        errorCondition("Samples in metadata don't match samples in object")
+    }
+    object
+}
+
+#' A function to return the data frames holding variant calls.
+#' Can optionally return a subset of the callers defined by a vector
+#'
+#' @title return_callers
+#' @return List of dataframes containing variant call information from all/selected callers.
+#' @param object object of class VariantHelper
+#' @param callers vector of the names of variant callers stored in the Callers slot.
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' return_callers(obj, metadata)
+
+return_callers <- function(object, callers = NULL, ...) {
+    if (is.null(callers)) {
+        return(object@Callers)
+    } else {
+        return(object@Callers[callers])
+    }
+}
+
+#' A function to take data from selected callers and combine them into a long dataframe
+#'
+#' @title combine_calls
+#' @return Long data frame of calls from all the callers.
+#' @param object object of class VariantHelper
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' combine_calls(obj)
+#'
+#' combine_calls(obj)
+
+combine_calls <- function(object, ...) {
+    return_callers(object, ...) %>%
+        bind_rows(.id = "Caller") %>%
+        left_join(object@meta.data, ., by = "Sample") %>%
+        unite("varID", c(CHROM, POS, REF, ALT), remove = FALSE)
+}
+
+#' A function to plot the number of variants caller per sample (or other metadata column) per caller.
+#'
+#' @title variants_by_caller
+#' @return ggplot of the number of variants.
+#' @param object object of class VariantHelper
+#' @param category name of a column in the metadata (default: Sample)
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' variants_by_caller(obj)
+
+variants_by_caller <- function(object, category = Sample, ...) {
+    combine_calls(object, ...) %>%
+        ggplot(aes(x = {{category}}, fill = {{category}})) +
+        geom_bar() +
+        facet_wrap(~ Caller) +
+        scale_x_discrete(guide = guide_axis(angle = 90))
+}
+
+#' A function to plot the chosen statistic for each variant per sample (or other metadata column) per caller.
+#'
+#' @title stats_by_caller
+#' @return ggplot of the chose statistic of variants.
+#' @param object object of class VariantHelper
+#' @param category name of a column in the metadata (default: Sample)
+#' @keywords VCF
+#' @export
+#' @examples
+#'
+#' stats_by_caller(obj)
+
+stats_by_caller <- function(object, Stat = AF,  category = Sample, ...) {
+    combine_calls(object, ...) %>%
+        ggplot(aes(x = {{Stat}}, y = {{category}},
+                   colour = {{category}}, fill = {{category}})) +
+        ggridges::geom_density_ridges(alpha = 0.25) +
+        facet_wrap(~Caller) +
+        theme_classic()
+}
